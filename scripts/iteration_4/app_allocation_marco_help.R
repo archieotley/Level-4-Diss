6683764db211060bb631d6d6fe0acb1e1574a126
Archie Otley <archieotley@Archies-MacBook-Pro.local>
2022-04-17 12:56:52 +0100

    read csv file, need to chnage from character data type in csv file

--- scripts/iteration_4/app_allocation_marco_help.R
+++ scripts/iteration_4/app_allocation_marco_help.R
@@ -1,3 +1,4 @@
+#library(readr)
 pacman::p_load(dplyr,tidyr,lubridate)
 n.rooms <- 7
 room.conc <- data.frame(conc.P=runif(n=7,min=0,max=700),
@@ -8,25 +9,27 @@ room.conc <- data.frame(conc.P=runif(n=7,min=0,max=700),
 
 clinicians <- data.frame(clinicians=c("Ian","Dan"),is.free=rep("Yes"),binary.is.free=1)
 
+#patient_roster <- read_csv("scripts/iteration_4/patient_roster.csv")
+#View(patient_roster)
 patient_roster<- read.csv("patient_roster.csv")
 
-
-patient.time.in <- df[["Appointment.Start.time"]]
+##TODO fix csv input
+patient.time.in <- patient_roster$Appointment.Start.Time
 
 #  c("09:00:00","09:03:00",
 #    "09:30:00","09:38:00",
 #    "10:00:00","10:30:00",
-#    "11:00:00","11:05:00",
+#   "11:00:00","11:05:00",
 #    "12:00:00","12:30:00",
 #    "14:30:00","15:30:00")
 
-patient.date.in <- df[["Appointment.start.date"]]
+patient.date.in <- patient_roster$Appointment.start.date #%>%
 
-appointment.length <- df[["Appointment.length"]]
+appointment.length <- patient_roster$Appointment.length #%>%
 
 #appointment.length <- c(runif(n=NROW(patient.time.in),min=10,max=90))
 
-patient.infection <-df[["Infection.type"]]
+patient.infection <-patient_roster$Infection.type #%>%
   
 #patient.infection <- sample(c("C","P","NA"),replace = TRUE,prob = c(1/2,1/3,1-1/2+1/3),size = NROW(patient.time.in))# c("P","P","NA","P","C","NA","C","P","NA","NA","C","P")
 
@@ -121,10 +124,28 @@ while(sum(patient.roster$binary.seen.yet,na.rm=TRUE)<12){
             
             #FIXME Placeholder for the ODE but for the moment C just decrease by some random value
             #FIXME Placeholder for the ODE but for the moment P just increases by some random value
+            V<- 31.9+(runif(1)*11.8)
+            lambda<- runif(1, min = 30, max = 90) 
+            logE <- runif(1,min = 8, max = 13)
+            E <- (10^(logE))/3600
             
             room.conc %>% 
-              mutate(across(conc.C,~.x*runif(1,min=0,max=1))) %>% #reduces C but increases P. Currently reduces all the rooms even if they are used concurrently
-              mutate(across(conc.P,~.x/runif(1,min=0,max=1)))->room.conc
+              ## If C present mutate C
+              ## if P present mutate P
+              ##TODO fix if stuff
+              if(Infection.type[i]==C){
+                conc.C->E/(V*lambda)*(1-exp(-lambda*(appointment.length*3600)))
+                conc.P->C01*exp(-lambda*appointment.length*3600)
+                } 
+                else if (Infection.type[i]==P){
+                  conc.P->E/(V*lambda)*(1-exp(-lambda*(appointment.length*3600)))
+                  conc.C->C01*exp(-lambda*appointment.length*3600)
+                  }
+                    else {}
+              #if(NROW(Infection.type)==C |conc.C->E/(V*lambda)*(1-exp(-lambda*(appointment.length*3600))))
+              #if(NROW(Infection.type)==P |conc.P->E/(V*lambda)*(1-exp(-lambda*(appointment.length*3600))))
+             # mutate(across(conc.C,~.x*runif(1,min=0,max=1))) %>% #reduces C but increases P. Currently reduces all the rooms even if they are used concurrently
+             # mutate(across(conc.P,~.x/runif(1,min=0,max=1)))->room.conc
             
             #Stores final concentrations of the room after each patient
             room.conc.storage %>% 
@@ -244,8 +265,10 @@ while(sum(patient.roster$binary.seen.yet,na.rm=TRUE)<12){
           arrange(desc(conc.P))->r.temp.conc
         
         #FIXME forcing patient into next available room if the best room is occupied
+        # AGO adding in error message to resechule appointments
         if(i>1& NROW(r.temp.conc)>0){
           if(patient.roster$room[i-1]==r.temp.conc %>% slice(n()) %>% pull(room.letter) & patient.roster$clash[i]=="Wait"){
+             stop("Schedule is unviable, please reschedule appointments")}
             
             #next #Don't need to skip, just need to put patient in next best room
             if(NROW(r.temp.conc)==0|sum(clinicians$binary.is.free)<1){
@@ -425,7 +448,7 @@ while(sum(patient.roster$binary.seen.yet,na.rm=TRUE)<12){
       print("Still trying to fit patients in")
     }
     
-  }
+ # }
   
 }
 patient.roster
@@ -444,19 +467,3 @@ room.conc.storage
 # else allocate any free
 # end
 # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
