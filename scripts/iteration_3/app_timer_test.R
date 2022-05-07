b61d1e418f6caa509273d674670ac6893c3b8555
Archie Otley <archieotley@Archies-MacBook-Pro.local>
2022-05-07 10:48:40 +0100

    File Management

--- scripts/iteration_3/app_timer_test.R
+++ scripts/iteration_3/app_timer_test.R
@@ -1,9 +1,12 @@
-
+### input values
 
 E=runif(min = 0,max = 20,10)
 lambda=runif(min = 0,max = 2,10)
 V=30
 C=runif(min = 0,max = 200000,10)
+
+### find concentration when "safe", could be done better way
+
 
   Cs<-0.02*C
   
@@ -12,7 +15,11 @@ C=runif(min = 0,max = 200000,10)
   return(Ts)
  }  
  
+### Calculate time till safe
+ 
  p<-c()
+ 
+### 10 iterations
  
 for (i in 1:10) {
   p[i]<-time_safe(Cs,C,E[i],lambda,V)
@@ -20,6 +27,8 @@ for (i in 1:10) {
 }
  #time_safe(Cs,C,E,lambda,V)
 
+ ### Plot time till safe
+ 
  p<-log((Cs-E/(lambda*V))/(C-E/(lambda*V)))/(-lambda)
 boxplot(p) 
 
